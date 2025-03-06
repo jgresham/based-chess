b876469e0a5b1ff3f65dcc6f33eb4d0b1a5f6500
jgresham <johnsgresham@gmail.com>
2025-03-06 10:45:23 -0800

    initial dev working version of nextjs conversion

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	// todo: remove after initial testing
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+};
+
+export default nextConfig;
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
---
+++ src/app/providers.tsx
@@ -0,0 +1,37 @@
+"use client";
+import FrameProvider from "../lib/frameProvider";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import sdk, { type Context } from "@farcaster/frame-sdk";
+import { useState, useEffect } from "react";
+import { darkTheme } from "@rainbow-me/rainbowkit";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+
+  const [isSDKLoaded, setIsSDKLoaded] = useState(false);
+  const [context, setContext] = useState<Context.FrameContext>();
+
+  useEffect(() => {
+    const load = async () => {
+      const context = await sdk.context;
+      setContext(context);
+      console.log("Calling sdk.actions.ready()");
+      sdk.actions.ready();
+    }
+    if (sdk && !isSDKLoaded) {
+      console.log("Calling load");
+      setIsSDKLoaded(true);
+      load();
+      return () => {
+        sdk.removeAllListeners();
+      };
+    }
+  }, [isSDKLoaded]);
+
+  return (
+    <FrameProvider>
+      <RainbowKitProvider theme={darkTheme()}>
+        {children}
+      </RainbowKitProvider>
+    </FrameProvider>
+  );
+}
