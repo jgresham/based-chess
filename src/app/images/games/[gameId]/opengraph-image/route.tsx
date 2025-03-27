ef2f9edb0f802a9ac51d538c779ddfeac323a153
jgresham <johnsgresham@gmail.com>
2025-03-26 20:09:54 -0700

    resize og image for warpcast. utc url param avoid caching

--- src/app/images/games/[gameId]/opengraph-image/route.tsx
+++ src/app/images/games/[gameId]/opengraph-image/route.tsx
@@ -14,8 +14,8 @@ const truncateAddress = (address: `0x${string}` | undefined) => {
 
 // 1.5 ratio?
 // 478 x 320 on web tool
-const WIDTH = 900;
-const HEIGHT = 600;
+export const WIDTH = 900;
+export const HEIGHT = 600;
 
 // export default async function handler(request: NextRequest, { params }: { params: { gameId: string } }) {
 export async function GET(request: NextRequest, { params }: { params: { gameId: string } }) {
@@ -24,43 +24,45 @@ export async function GET(request: NextRequest, { params }: { params: { gameId: 
   // get the cloudflaredurable object using gameId to get the game details
   console.log("getting game durable object");
   const cf = await getCloudflareContext();
-  const durObjId = await cf.env.CHESS_GAME.idFromName(gameId);
-  const durObjStub = cf.env.CHESS_GAME.get(durObjId);
-  const gameData = await durObjStub.getUserFacingGameData();
-  // const gameData = {
-  //   player1Address: '0x7D20fd2BD3D13B03571A36568cfCc2A4EB3c749e',
-  //   player2Address: '0x2a99EC82d658F7a77DdEbFd83D0f8F591769cB64',
-  //   gameId: '6VQ3KF',
-  //   liveViewers: 1,
-  //   latestPlayer1Signature: '0xca7507bf6533a781246ee647c4c9e2063370153fbd11c3aaa0e8365bcdbd64137478ccbf26f0806f1771ab2d74863bf8d8db1a6a2cc51046e1bfe388062d75fc1c',
-  //   latestPlayer1Message: '[White "0x7D20fd2BD3D13B03571A36568cfCc2A4EB3c749e"]\n' +
-  //     '[Black "0x2a99EC82d658F7a77DdEbFd83D0f8F591769cB64"]\n' +
-  //     '[Event "12,6VQ3KF"]\n' +
-  //     '[Site "Based Chess"]\n' +
-  //     '[Date "2025-03-06T22:30:37.454Z"]\n' +
-  //     '\n' +
-  //     '1. e3 f6 2. Nc3 g5 3. Qh5#',
-  //   latestPlayer2Signature: '0x581fcce02f1caefb8c324f51d1a62e815ad19fce4fc9ea1ec00d1b436711f85f072cafaebe18ffbd201f72be030466599dc9af2e0edb04da47a52f9ccefeb8a71c',
-  //   latestPlayer2Message: '[White "0x7D20fd2BD3D13B03571A36568cfCc2A4EB3c749e"]\n' +
-  //     '[Black "0x2a99EC82d658F7a77DdEbFd83D0f8F591769cB64"]\n' +
-  //     '[Event "12,6VQ3KF"]\n' +
-  //     '[Site "Based Chess"]\n' +
-  //     '[Date "2025-03-06T22:30:37.454Z"]\n' +
-  //     '\n' +
-  //     '1. e3 f6 2. Nc3 g5',
-  //   createdTimestamp: 1741300237454,
-  //   contractGameId: 12,
-  //   pgn: '[White "0x7D20fd2BD3D13B03571A36568cfCc2A4EB3c749e"]\n' +
-  //     '[Black "0x2a99EC82d658F7a77DdEbFd83D0f8F591769cB64"]\n' +
-  //     '[Event "12,6VQ3KF"]\n' +
-  //     '[Site "Based Chess"]\n' +
-  //     '[Date "2025-03-06T22:30:37.454Z"]\n' +
-  //     '\n' +
-  //     // '1. e3 f6 2. Nc3 g5 3. Qh5#'
-  //     '1. e3 f6 2. Nc3 g5 3. Qh5#'
-  // }
-
-  // gameData.pgn = '1. e3 f6 2. Nc3 g5 3. Qh5#';
+  let gameData = null;
+  if (process.env.NODE_ENV === 'development') {
+    gameData = {
+      player1Address: '0x7D20fd2BD3D13B03571A36568cfCc2A4EB3c749e',
+      player2Address: '0x2a99EC82d658F7a77DdEbFd83D0f8F591769cB64',
+      gameId: '6VQ3KF',
+      liveViewers: 1,
+      latestPlayer1Signature: '0xca7507bf6533a781246ee647c4c9e2063370153fbd11c3aaa0e8365bcdbd64137478ccbf26f0806f1771ab2d74863bf8d8db1a6a2cc51046e1bfe388062d75fc1c',
+      latestPlayer1Message: '[White "0x7D20fd2BD3D13B03571A36568cfCc2A4EB3c749e"]\n' +
+        '[Black "0x2a99EC82d658F7a77DdEbFd83D0f8F591769cB64"]\n' +
+        '[Event "12,6VQ3KF"]\n' +
+        '[Site "Based Chess"]\n' +
+        '[Date "2025-03-06T22:30:37.454Z"]\n' +
+        '\n' +
+        '1. e3 f6 2. Nc3 g5 3. Qh5#',
+      latestPlayer2Signature: '0x581fcce02f1caefb8c324f51d1a62e815ad19fce4fc9ea1ec00d1b436711f85f072cafaebe18ffbd201f72be030466599dc9af2e0edb04da47a52f9ccefeb8a71c',
+      latestPlayer2Message: '[White "0x7D20fd2BD3D13B03571A36568cfCc2A4EB3c749e"]\n' +
+        '[Black "0x2a99EC82d658F7a77DdEbFd83D0f8F591769cB64"]\n' +
+        '[Event "12,6VQ3KF"]\n' +
+        '[Site "Based Chess"]\n' +
+        '[Date "2025-03-06T22:30:37.454Z"]\n' +
+        '\n' +
+        '1. e3 f6 2. Nc3 g5',
+      createdTimestamp: 1741300237454,
+      contractGameId: 12,
+      pgn: '[White "0x7D20fd2BD3D13B03571A36568cfCc2A4EB3c749e"]\n' +
+        '[Black "0x2a99EC82d658F7a77DdEbFd83D0f8F591769cB64"]\n' +
+        '[Event "12,6VQ3KF"]\n' +
+        '[Site "Based Chess"]\n' +
+        '[Date "2025-03-06T22:30:37.454Z"]\n' +
+        '\n' +
+        // '1. e3 f6 2. Nc3 g5 3. Qh5#'
+        '1. e3 f6 2. Nc3 g5 3. Qh5#'
+    }
+  } else {
+    const durObjId = await cf.env.CHESS_GAME.idFromName(gameId);
+    const durObjStub = cf.env.CHESS_GAME.get(durObjId);
+    gameData = await durObjStub.getUserFacingGameData();
+  }
 
   console.log("gameData", gameData);
   if (!gameData || !gameData.player1Address || !gameData.player2Address) {
@@ -110,7 +112,7 @@ export async function GET(request: NextRequest, { params }: { params: { gameId: 
     // </div>
     <div tw="w-[900px] h-[600px] bg-gray-800 flex flex-col font-sans text-white">
       {/* <!-- Header --> */}
-      <div tw="h-38 flex items-center justify-between px-8">
+      <div tw="h-38 flex items-center justify-between px-8 pt-24">
         <div tw="flex flex-row items-center gap-8">
           {/* <!-- Replace with your actual logo path --> */}
           <img src="https://basedchess.xyz/based-chess-logo-200.jpg" alt="Company Logo" tw="h-24 mr-10" />
