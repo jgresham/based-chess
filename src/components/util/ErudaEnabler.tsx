3f4a391a245789bb646abb8fb9953b5b04d7ffcb
jgresham <johnsgresham@gmail.com>
2025-04-04 11:27:31 -0700

    enabled eruda mobile logs in dev mode

---
+++ src/components/util/ErudaEnabler.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useDevMode } from "../hooks/useLocalSettings";
+import eruda from "eruda";
+
+export const ErudaEnabler = () => {
+	const [isMounted, setIsMounted] = useState(false);
+	const [isInitialized, setIsInitialized] = useState(false);
+	const { data: isDevMode } = useDevMode();
+
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+
+	useEffect(() => {
+		// if (isDevMode || process.env.NODE_ENV !== "production") {
+		if (isDevMode && isMounted) {
+			console.log("initializing eruda");
+			import("eruda").then((eruda) => {
+				eruda.default.init();
+				setIsInitialized(true);
+			});
+		} else {
+			if (isInitialized) {
+				console.log("destroying eruda");
+				try {
+					if (eruda !== undefined) {
+						eruda.destroy();
+					}
+				} catch (error) {
+					console.error("error destroying eruda", error);
+				}
+			}
+		}
+	}, [isDevMode, isMounted, isInitialized]);
+
+	return <></>;
+};
